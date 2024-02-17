bf239cdab485b3b4159cf3a0fb9f9810329b16ab
Victorien Druon <71207520+VictorienDruon@users.noreply.github.com>
2024-02-17 12:14:18 +0100

    Initial Commit

---
+++ MovieNetwork/MovieNetworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieNetworkApp.swift
+//  MovieNetwork
+//
+//  Created by Victorien Druon on 17/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieNetworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
